a5b64c1d725c4a89d839cc2799d8d61d16affb9f
HunterkwesT <alexandre.pereira460@gmail.com>
2025-08-18 17:46:45 -0300

    att

--- GPT-5/Modulo 5 - Manipulação de arquivos/1 - Criar arquivo.py
+++ GPT-5/Modulo 5 - Manipulação de arquivos/1 - Criar arquivo.py
@@ -1,3 +1,3 @@
 with open("Modulo 5 - exercicio 1.txt","w") as arquivo:
     for i in range(3):
-        arquivo.write(input(f"Nome {i+1}: ")+"\n")
+        arquivo.write(input(f"Nome {i+1}: ").strip()+"\n")
---
+++ GPT-5/Modulo 6 - JSON/1 - Salvar Dict em Json.py
@@ -0,0 +1,5 @@
+dados = (
+    "nome":,
+)
+
+with open("Modulo 6 - exercicio 1.json","w",encoding="utf-8") as arquivo:
